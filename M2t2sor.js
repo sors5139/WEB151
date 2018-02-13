1c921784dfbef9f606e00c3994924ef56781ee1b
sors5139 <35981180+sors5139@users.noreply.github.com>
2018-02-13 12:25:52 -0500

    UPLOADED FOR M2T

---
+++ M2t2sor.js
@@ -0,0 +1,16 @@
+console.log('sausage');
+console.log('dragon');
+console.log('JavaScript');
+console.log(33.7);
+console.log(true);
+console.log(null);
+console.log(3.5 + 28);
+console.log(2017 - 1969);
+console.log(65/240);
+console.log(0.2708*100);
+console.log('Teaching the world how to code'.length);
+console.log('CODECADEMY'.toUpperCase());
+console.log(Math.random()*100);
+Math.floor(Math.random()*Math.floor()*100);
+// Opening Line.
+/* console.log('To the right, we've provided you with the beginning of the book Catch-22 by Joseph Heller.);*/
